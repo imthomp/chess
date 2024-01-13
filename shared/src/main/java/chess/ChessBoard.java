a0b3c500746fd4bf55b3557fadaa577ee988bd27
Isaac <it238@byu.edu>
2024-01-12 18:06:27 -0700

    implement ChessBoard array, getPiece, getRow, getColumn

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -7,7 +7,7 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
-z
+    private ChessPiece[][] squares = new ChessPiece[8][8];
     public ChessBoard() {
 
     }
@@ -19,7 +19,7 @@ z
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        squares[position.getRow()][position.getColumn()] = piece;
     }
 
     /**
@@ -30,7 +30,7 @@ z
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return squares[position.getRow()][position.getColumn()];
     }
 
     /**
