52e228406c625a61913c417e7f4e7dab52603cfe
Isaac <it238@byu.edu>
2024-01-15 15:40:34 -0700

    optimize shared functionality of Queen, Bishop, Rook

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * Represents a single chess piece
@@ -53,29 +54,87 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        ArrayList<ChessMove> moves = new ArrayList<ChessMove>();
+        HashSet<ChessMove> moves = new HashSet<>();
         if (type == PieceType.KING) {
-            for (int rowOffset = -1; rowOffset <= 1; rowOffset++) {
-                for (int colOffset = -1; colOffset <= 1; colOffset++) {
-                    if (rowOffset != 0 || colOffset != 0) {
-                        ChessPosition destination = new ChessPosition(myPosition.getRow() + rowOffset, myPosition.getColumn() + colOffset);
-                        moves.add(new ChessMove(myPosition, destination, null));
-                        // Check if the destination is valid
-                        // Check if the destination is not occupied by a friendly piece
-                    }
+            // King can move one square in any direction
+            int[] rowOffsets = {-1, -1, -1, 0, 0, 1, 1, 1};
+            int[] colOffsets = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+            for (int i = 0; i < rowOffsets.length; i++) {
+                int rowOffset = rowOffsets[i];
+                int colOffset = colOffsets[i];
+
+                ChessPosition destination = new ChessPosition(myPosition.getRow() + rowOffset, myPosition.getColumn() + colOffset);
+
+                // Check if the destination is valid and not occupied by a friendly piece
+                if (board.isValidPosition(destination) && (board.getPiece(destination) == null || board.getPiece(destination).getTeamColor() != getTeamColor())) {
+                    moves.add(new ChessMove(myPosition, destination, null));
                 }
             }
         } else if (type == PieceType.QUEEN) {
-            throw new RuntimeException("Not implemented");
+            // Queen can move vertically, horizontally, and diagonally any number of squares
+            // Combine the logic of rook and bishop
+            // Rook-like movement
+            getRookMoves(board, myPosition, moves);
+            // Bishop-like movement
+            getBishopMoves(board, myPosition, moves);
         } else if (type == PieceType.BISHOP) {
-            throw new RuntimeException("Not implemented");
+            // Bishop can move diagonally any number of squares
+            getBishopMoves(board, myPosition, moves);
         } else if (type == PieceType.KNIGHT) {
             throw new RuntimeException("Not implemented");
         } else if (type == PieceType.ROOK) {
-            throw new RuntimeException("Not implemented");
+            // Rook can move vertically or horizontally any number of squares
+            getRookMoves(board, myPosition, moves);
         } else if (type == PieceType.PAWN) {
             throw new RuntimeException("Not implemented");
         }
         return moves;
+    }
+
+    private void getBishopMoves(ChessBoard board, ChessPosition myPosition, HashSet<ChessMove> moves) {
+        int[] rowOffsets = {-1, -1, 1, 1};
+        int[] colOffsets = {-1, 1, -1, 1};
+
+        for (int i = 0; i < rowOffsets.length; i++) {
+            int rowOffset = rowOffsets[i];
+            int colOffset = colOffsets[i];
+
+            ChessPosition destination = new ChessPosition(myPosition.getRow() + rowOffset, myPosition.getColumn() + colOffset);
+            addMovesUntilBlocked(board, myPosition, destination, moves);
+        }
+    }
+
+    private void getRookMoves(ChessBoard board, ChessPosition myPosition, HashSet<ChessMove> moves) {
+        for (int rowOffset = -1; rowOffset <= 1; rowOffset++) {
+            if (rowOffset != 0) {
+                ChessPosition destination = new ChessPosition(myPosition.getRow() + rowOffset, myPosition.getColumn());
+                addMovesUntilBlocked(board, myPosition, destination, moves);
+            }
+        }
+        for (int colOffset = -1; colOffset <= 1; colOffset++) {
+            if (colOffset != 0) {
+                ChessPosition destination = new ChessPosition(myPosition.getRow(), myPosition.getColumn() + colOffset);
+                addMovesUntilBlocked(board, myPosition, destination, moves);
+            }
+        }
+    }
+
+    private void addMovesUntilBlocked(ChessBoard board, ChessPosition myPosition, ChessPosition destination, HashSet<ChessMove> moves) {
+        while (board.isValidPosition(destination)) {
+            ChessPiece pieceAtDestination = board.getPiece(destination);
+
+            if (pieceAtDestination == null) {
+                moves.add(new ChessMove(myPosition, destination, null));
+            } else if (pieceAtDestination.getTeamColor() != getTeamColor()) {
+                moves.add(new ChessMove(myPosition, destination, null));
+                break;  // Capture opponent's piece, then stop the movement
+            } else {
+                break;  // Friendly piece blocking the path
+            }
+
+            destination = new ChessPosition(destination.getRow() + Integer.signum(myPosition.getRow() - destination.getRow()),
+                    destination.getColumn() + Integer.signum(myPosition.getColumn() - destination.getColumn()));
+        }
     }
 }
