99a9425c15e3c04b429d90b40fa54f8f9bc9b216
Isaac <it238@byu.edu>
2024-01-20 17:07:11 -0700

    convert ChessMove to record

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -8,30 +8,7 @@ import java.util.Objects;
  * Note: You can add to this class, but you may not alter
  * signature of the existing methods.
  */
-public class ChessMove {
-
-    private final ChessPosition startPosition;
-    private final ChessPosition endPosition;
-    private final ChessPiece.PieceType promotionPiece;
-
-    public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
-                     ChessPiece.PieceType promotionPiece) {
-        this.startPosition = startPosition;
-        this.endPosition = endPosition;
-        this.promotionPiece = promotionPiece;
-    }
-
-    public ChessPosition getStartPosition() {
-        return startPosition;
-    }
-
-    public ChessPosition getEndPosition() {
-        return endPosition;
-    }
-
-    public ChessPiece.PieceType getPromotionPiece() {
-        return promotionPiece;
-    }
+public record ChessMove(ChessPosition startPosition, ChessPosition endPosition, ChessPiece.PieceType promotionPiece) {
 
     @Override
     public boolean equals(Object o) {
@@ -39,11 +16,6 @@ public class ChessMove {
         if (o == null || getClass() != o.getClass()) return false;
         ChessMove chessMove = (ChessMove) o;
         return Objects.equals(startPosition, chessMove.startPosition) && Objects.equals(endPosition, chessMove.endPosition) && promotionPiece == chessMove.promotionPiece;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(startPosition, endPosition, promotionPiece);
     }
 
     @Override
