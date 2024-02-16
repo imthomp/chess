f7a8e39144fed93c4bca5bb4182653d79bd2d351
Isaac <it238@byu.edu>
2024-02-15 21:32:03 -0700

    add ClearService functionality

--- server/src/main/java/handler/ClearHandler.java
+++ server/src/main/java/handler/ClearHandler.java
@@ -1,4 +1,22 @@
 package handler;
 
+import com.google.gson.Gson;
+import result.ClearResult;
+import service.ClearService;
+import spark.Request;
+import spark.Response;
+
 public class ClearHandler {
+    public Object handleClear(Request req, Response res) {
+        ClearService service = new ClearService();
+        ClearResult result = service.clear();
+        if (result != null) {
+            var serializer = new Gson();
+            var json = serializer.toJson(result);
+            res.status(500);
+            return json;
+        }
+        res.status(200);
+        return "";
+    }
 }
--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,4 +1,23 @@
 package service;
 
+import dataAccess.AuthDAO;
+import dataAccess.DataAccessException;
+import dataAccess.GameDAO;
+import dataAccess.UserDAO;
+import result.ClearResult;
+
 public class ClearService {
+    public ClearResult clear() {
+        try {
+            UserDAO userDAO = new UserDAO();
+            userDAO.clear();
+            AuthDAO authDAO = new AuthDAO();
+            authDAO.clear();
+            GameDAO gameDAO = new GameDAO();
+            gameDAO.clear();
+        } catch (DataAccessException e) {
+            String message = e.getMessage();
+            return new ClearResult(message);
+        }
+    }
 }
