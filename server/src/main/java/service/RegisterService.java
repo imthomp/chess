b8bb7e501ee2387953b3d21a0cddb9af02575b13
Isaac <it238@byu.edu>
2024-02-22 07:00:23 -0700

    begin to implement RegisterService

--- server/src/main/java/service/RegisterService.java
+++ server/src/main/java/service/RegisterService.java
@@ -1,4 +1,37 @@
 package service;
 
+import dataAccess.DataAccessException;
+import dataAccess.UserDAO;
+import model.UserData;
+import result.RegisterResult;
+import dataAccess.MemoryUserDAO;
+
 public class RegisterService {
+    public RegisterResult getUser(String username) {
+        try {
+            UserDAO userDAO = new MemoryUserDAO();
+            UserData u = userDAO.getUser(username);
+            return new RegisterResult("");
+        } catch (DataAccessException e) {
+            String message = e.getMessage();
+            return new RegisterResult(message);
+        }
+    }
+
+    public RegisterResult createUser(UserData u) {
+        try {
+            UserDAO userDAO = new MemoryUserDAO();
+            if (getUser(u.username()) == null) {
+                userDAO.createUser(u);
+            }
+            return new RegisterResult("");
+        } catch (DataAccessException e) {
+            String message = e.getMessage();
+            return new RegisterResult(message);
+        }
+    }
+
+    public RegisterResult createAuth() {
+        return new RegisterResult("");
+    }
 }
