d54cd72a8489481eed359a0d373c00184a65f905
Isaac <it238@byu.edu>
2024-02-22 06:57:09 -0700

    getUser returns UserData

--- server/src/main/java/dataAccess/UserDAO.java
+++ server/src/main/java/dataAccess/UserDAO.java
@@ -4,6 +4,6 @@ import model.UserData;
 
 public interface UserDAO {
     void createUser(UserData u) throws DataAccessException;
-    void getUser(String username) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
     void clear() throws DataAccessException;
 }
