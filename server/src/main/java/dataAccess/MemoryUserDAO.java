2a1e8e6966fa07c87e189d29fe84955920acc605
Isaac <it238@byu.edu>
2024-02-22 06:59:03 -0700

    implement createUser, getUser

--- server/src/main/java/dataAccess/MemoryUserDAO.java
+++ server/src/main/java/dataAccess/MemoryUserDAO.java
@@ -9,9 +9,11 @@ public class MemoryUserDAO implements UserDAO {
     HashMap<String,UserData> users;
     public void createUser(UserData u) throws DataAccessException {
         // Insert user into database
+        users.put(u.username(), u);
     }
-    public void getUser(String username) throws DataAccessException {
+    public UserData getUser(String username) throws DataAccessException {
         // Get user from database
+        return users.get(username);
     }
     public void clear() throws DataAccessException {
         // Clear database
