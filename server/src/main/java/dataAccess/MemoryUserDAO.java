bde9d396daa8373c2511b42f2b398d51819b740f
Isaac <it238@byu.edu>
2024-02-26 18:02:36 -0700

    implement MemoryUserDAO functionality

--- server/src/main/java/dataAccess/MemoryUserDAO.java
+++ server/src/main/java/dataAccess/MemoryUserDAO.java
@@ -1,22 +1,41 @@
 package dataAccess;
 
+import chess.ChessBoard;
+import chess.ChessGame;
 import model.UserData;
 
+import javax.xml.crypto.Data;
 import java.util.Collection;
 import java.util.HashMap;
 
 public class MemoryUserDAO implements UserDAO {
-    HashMap<String,UserData> users;
+    private HashMap<String,UserData> users;
+
+    public MemoryUserDAO() {
+        users = new HashMap<String,UserData>();
+    }
+
     public void createUser(UserData u) throws DataAccessException {
         // Insert user into database
         users.put(u.username(), u);
     }
-    public UserData getUser(String username) throws DataAccessException {
+
+    public UserData getUser(UserData u) throws DataAccessException {
         // Get user from database
-        return users.get(username);
+        if (users == null) {
+            return null;
+        }
+        return users.get(u.username());
     }
+
+    public boolean checkUserCredentials(UserData u) throws DataAccessException {
+        // TODO
+        return false;
+    }
+
     public void clear() throws DataAccessException {
         // Clear database
         users = new HashMap<String,UserData>();
     }
+
 }
--- server/src/main/java/dataAccess/UserDAO.java
+++ server/src/main/java/dataAccess/UserDAO.java
@@ -2,8 +2,11 @@ package dataAccess;
 
 import model.UserData;
 
+import javax.xml.crypto.Data;
+
 public interface UserDAO {
     void createUser(UserData u) throws DataAccessException;
-    UserData getUser(String username) throws DataAccessException;
+    UserData getUser(UserData u) throws DataAccessException;
+    boolean checkUserCredentials(UserData u) throws DataAccessException;
     void clear() throws DataAccessException;
 }
