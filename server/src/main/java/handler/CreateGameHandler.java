209e26f4ef1856e02124a128d39484c808569fee
Isaac <it238@byu.edu>
2024-02-27 06:59:13 -0700

    begin CreateGame endpoint

--- server/src/main/java/handler/CreateGameHandler.java
+++ server/src/main/java/handler/CreateGameHandler.java
@@ -1,4 +1,55 @@
 package handler;
 
+import dataAccess.object.protocol.AuthDAO;
+import dataAccess.object.protocol.GameDAO;
+import result.GameResult;
+import service.CreateGameService;
+
+import com.google.gson.Gson;
+import spark.Request;
+import spark.Response;
+
+import java.util.Objects;
+
 public class CreateGameHandler {
+    private final AuthDAO authDAO;
+    private final GameDAO gameDAO;
+
+    public CreateGameHandler (AuthDAO authDAO, GameDAO gameDAO) {
+        this.authDAO = authDAO;
+        this.gameDAO = gameDAO;
+    }
+
+    public Object handleCreateGame(Request req, Response res) {
+        CreateGameService service = new CreateGameService(authDAO, gameDAO);
+
+        var serializer = new Gson();
+
+        String authToken = serializer.fromJson(req.body(), String.class);
+        String gameName = serializer.fromJson(req.body(), String.class);
+        GameResult result = service.createGame(authToken, gameName);
+
+        var json = serializer.toJson(result);
+
+        // Success response [200]
+        if (result.message() == null) {
+            res.status(200);
+        }
+
+        // Failure response [400] { "message": "Error: bad request" }
+        if (Objects.equals(result.message(), "bad request")) {
+            res.status(400);
+        }
+
+        // Failure response [401] { "message": "Error: unauthorized" }
+        if (Objects.equals(result.message(), "unauthorized")) {
+            res.status(401);
+        }
+
+        // Failure response	[500] { "message": "Error: description" }
+        else {
+            res.status(500);
+        }
+        return json;
+    }
 }
--- server/src/main/java/service/CreateGameService.java
+++ server/src/main/java/service/CreateGameService.java
@@ -1,4 +1,29 @@
 package service;
 
+import dataAccess.exception.DataAccessException;
+import dataAccess.object.protocol.AuthDAO;
+import dataAccess.object.protocol.GameDAO;
+import model.AuthData;
+import model.GameData;
+import result.GameResult;
+
 public class CreateGameService {
+    private final AuthDAO authDAO;
+    private final GameDAO gameDAO;
+
+    public CreateGameService (AuthDAO authDAO, GameDAO gameDAO) {
+        this.authDAO = authDAO;
+        this.gameDAO = gameDAO;
+    }
+    public GameResult createGame(String authToken, String gameName) {
+        try {
+            AuthData user = authDAO.getAuth(authToken);
+            String username = user.username();
+            GameData game = gameDAO.createGame(authToken, gameName);
+            return new GameResult(game.gameID(), authToken);
+        } catch (DataAccessException e) {
+            String message = e.getMessage();
+            return new GameResult(null, "Error:" + message);
+        }
+    }
 }
