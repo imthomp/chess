6789793565b6f0691aac0fab6a50ca07209bf2b6
Isaac <it238@byu.edu>
2024-02-26 18:05:25 -0700

    implement Logout endpoint

--- server/src/main/java/handler/LogoutHandler.java
+++ server/src/main/java/handler/LogoutHandler.java
@@ -1,4 +1,44 @@
 package handler;
 
+import com.google.gson.Gson;
+import dataAccess.AuthDAO;
+import result.MessageResult;
+import service.LogoutService;
+import spark.Request;
+import spark.Response;
+
+import java.util.Objects;
+
 public class LogoutHandler {
+    private final AuthDAO authDAO;
+
+    public LogoutHandler (AuthDAO authDAO) {
+        this.authDAO = authDAO;
+    }
+    public Object handleLogout(Request req, Response res) {
+        LogoutService service = new LogoutService(authDAO);
+        var serializer = new Gson();
+        String authToken = serializer.fromJson(req.body(), String.class);
+        MessageResult result = service.logout(authToken);
+
+        var json = serializer.toJson(result);
+
+        // Success response	[200]
+        if (result.message() == null || result.message().isEmpty()) {
+            res.status(200);
+        }
+
+        // Failure response	[401] { "message": "Error: unauthorized" }
+        if (Objects.equals(result.message(), "unauthorized")) {
+            // TODO properly throw error somewhere
+            res.status(400);
+        }
+
+        // Failure response	[500] { "message": "Error: description" }
+        else {
+            // TODO properly throw error somewhere
+            res.status(500);
+        }
+        return json;
+    }
 }
--- server/src/main/java/service/LogoutService.java
+++ server/src/main/java/service/LogoutService.java
@@ -1,4 +1,21 @@
 package service;
 
+import dataAccess.*;
+import result.MessageResult;
+
 public class LogoutService {
+    private final AuthDAO authDAO;
+
+    public LogoutService (AuthDAO authDAO) {
+        this.authDAO = authDAO;
+    }
+    public MessageResult logout(String authToken) {
+        try {
+            authDAO.deleteAuth(authToken);
+            return new MessageResult(null);
+        } catch (DataAccessException e) {
+            String message = e.getMessage();
+            return new MessageResult(message);
+        }
+    }
 }
