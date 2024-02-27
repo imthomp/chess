5e2833b068038323c12b53520716d95262c3e914
Isaac <it238@byu.edu>
2024-02-26 18:02:14 -0700

    implement MemoryAuthDAO functionality

--- server/src/main/java/dataAccess/AuthDAO.java
+++ server/src/main/java/dataAccess/AuthDAO.java
@@ -3,8 +3,8 @@ package dataAccess;
 import model.AuthData;
 
 public interface AuthDAO {
-    void createAuth(AuthData a) throws DataAccessException;
-    void getAuth(String authToken) throws DataAccessException;
+    String createAuth(String username) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
     void deleteAuth(String authToken) throws DataAccessException;
     void clear() throws DataAccessException;
 }
--- server/src/main/java/dataAccess/MemoryAuthDAO.java
+++ server/src/main/java/dataAccess/MemoryAuthDAO.java
@@ -1,22 +1,33 @@
 package dataAccess;
 
 import model.AuthData;
-import model.UserData;
-
-import java.util.Collection;
 import java.util.HashMap;
+import java.util.UUID;
 
 public class MemoryAuthDAO implements AuthDAO {
-    HashMap<String, AuthData> auths;
-    public void createAuth(AuthData a) throws DataAccessException {
+    private HashMap<String, AuthData> auths;
+
+    public MemoryAuthDAO() {
+        auths = new HashMap<String, AuthData>();
+    }
+
+    public String createAuth(String username) throws DataAccessException {
         // Insert auth into database
+        String authToken = UUID.randomUUID().toString();
+        auths.put(username, new AuthData(username, authToken));
+        return authToken;
     }
-    public void getAuth(String authToken) throws DataAccessException {
+
+    public AuthData getAuth(String authToken) throws DataAccessException {
         // Get auth from database
+        return auths.get(authToken);
     }
+
     public void deleteAuth(String authToken) throws DataAccessException {
         // Delete auth from database
+        auths.remove(authToken);
     }
+
     public void clear() throws DataAccessException {
         // Clear database
         auths = new HashMap<String, AuthData>();
