f292372db576437c1fb3fa627cb319267c51b6bb
Isaac <it238@byu.edu>
2024-02-26 18:00:31 -0700

    implement Login endpoint

--- server/src/main/java/handler/LoginHandler.java
+++ server/src/main/java/handler/LoginHandler.java
@@ -1,4 +1,49 @@
 package handler;
 
+import com.google.gson.Gson;
+import dataAccess.AuthDAO;
+import dataAccess.UserDAO;
+import model.UserData;
+import result.RegisterResult;
+import service.LoginService;
+import spark.Request;
+import spark.Response;
+import java.util.Objects;
+
 public class LoginHandler {
+    private final AuthDAO authDAO;
+    private final UserDAO userDAO;
+
+    public LoginHandler (AuthDAO authDAO, UserDAO userDAO) {
+        this.authDAO = authDAO;
+        this.userDAO = userDAO;
+    }
+    public Object handleLogin(Request req, Response res) {
+        LoginService service = new LoginService(authDAO, userDAO);
+        var serializer = new Gson();
+        UserData u = serializer.fromJson(req.body(), UserData.class);
+        RegisterResult result = service.login(u);
+        var json = serializer.toJson(result);
+
+        // Success response	[200] { "username":"", "authToken":"" }
+        if (result.message() == null || result.message().isEmpty()) {
+            res.status(200);
+            // TODO strip json of message
+            return json;
+        }
+
+        // Failure response	[400] { "message": "Error: unauthorized" }
+        if (Objects.equals(result.message(), "unauthorized")) {
+            // TODO properly throw error somewhere
+            res.status(400);
+        }
+
+        // Failure response	[500] { "message": "Error: description" }
+        else {
+            // TODO properly throw error somewhere
+            res.status(500);
+        }
+        // TODO strip json of username, authToken
+        return json;
+    }
 }
--- server/src/main/java/service/LoginService.java
+++ server/src/main/java/service/LoginService.java
@@ -1,4 +1,28 @@
 package service;
 
+import dataAccess.*;
+import model.UserData;
+import result.RegisterResult;
+
 public class LoginService {
+    private final AuthDAO authDAO;
+    private final UserDAO userDAO;
+
+    public LoginService (AuthDAO authDAO, UserDAO userDAO) {
+        this.authDAO = authDAO;
+        this.userDAO = userDAO;
+    }
+    public RegisterResult login(UserData u) {
+        try {
+            if (userDAO.checkUserCredentials(u)) {
+                String username = u.username();
+                String authToken = authDAO.createAuth(username);
+                return new RegisterResult(username, authToken, null);
+            }
+        } catch (DataAccessException e) {
+            String message = e.getMessage();
+            return new RegisterResult(null, null, "Error:" + message);
+        }
+        return null;
+    }
 }
