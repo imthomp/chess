6865cbfdd25a47149fd1bb87c0f3eb5dedc79e08
Isaac <it238@byu.edu>
2024-02-27 07:00:02 -0700

    create ListGames endpoint

--- server/src/main/java/handler/ListGamesHandler.java
+++ server/src/main/java/handler/ListGamesHandler.java
@@ -1,4 +1,49 @@
 package handler;
 
+import dataAccess.object.protocol.AuthDAO;
+import dataAccess.object.protocol.GameDAO;
+import result.ListGamesResult;
+import service.ListGamesService;
+
+import com.google.gson.Gson;
+import spark.Request;
+import spark.Response;
+
+import java.util.Objects;
+
 public class ListGamesHandler {
+    private final AuthDAO authDAO;
+    private final GameDAO gameDAO;
+
+    public ListGamesHandler (AuthDAO authDAO, GameDAO gameDAO) {
+        this.authDAO = authDAO;
+        this.gameDAO = gameDAO;
+    }
+
+    public Object handleListGames(Request req, Response res) {
+        ListGamesService service = new ListGamesService(authDAO, gameDAO);
+
+        var serializer = new Gson();
+
+        String authToken = serializer.fromJson(req.body(), String.class);
+        ListGamesResult result = service.listGames(authToken);
+
+        var json = serializer.toJson(result);
+
+        // Success response [200]
+        if (result.message() == null) {
+            res.status(200);
+        }
+
+        // Failure response [401] { "message": "Error: unauthorized" }
+        if (Objects.equals(result.message(), "unauthorized")) {
+            res.status(401);
+        }
+
+        // Failure response	[500] { "message": "Error: description" }
+        else {
+            res.status(500);
+        }
+        return json;
+    }
 }
--- server/src/main/java/service/ListGamesService.java
+++ server/src/main/java/service/ListGamesService.java
@@ -1,4 +1,37 @@
 package service;
 
-public class ListGamesService {
+import dataAccess.exception.DataAccessException;
+import dataAccess.object.protocol.AuthDAO;
+import dataAccess.object.protocol.GameDAO;
+import model.AuthData;
+import model.GameData;
+import result.ListGamesResult;
+
+import java.util.HashSet;
+
+public class ListGamesService  {
+    private final AuthDAO authDAO;
+    private final GameDAO gameDAO;
+
+    public ListGamesService (AuthDAO authDAO, GameDAO gameDAO) {
+        this.authDAO = authDAO;
+        this.gameDAO = gameDAO;
+    }
+
+    public ListGamesResult listGames(String authToken) {
+        try {
+            AuthData authData = authDAO.getAuth(authToken);
+            if (authData != null) {
+                String username = authData.username();
+                HashSet<GameData> games = gameDAO.listGames(username);
+                return new ListGamesResult(games, null);
+            }
+            else {
+                return new ListGamesResult(null, "Error: unauthorized");
+            }
+        } catch (DataAccessException e) {
+            String message = e.getMessage();
+            return new ListGamesResult(null, "Error:" + message);
+        }
+    }
 }
