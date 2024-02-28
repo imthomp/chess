685c500300bcc4a6a28eea4d70c52cbe06ae18af
Isaac <it238@byu.edu>
2024-02-27 19:07:50 -0700

    Collection instead of HashSet

--- server/src/main/java/dataAccess/object/protocol/GameDAO.java
+++ server/src/main/java/dataAccess/object/protocol/GameDAO.java
@@ -3,12 +3,12 @@ package dataAccess.object.protocol;
 import dataAccess.exception.DataAccessException;
 import model.GameData;
 
-import java.util.HashSet;
+import java.util.Collection;
 
 public interface GameDAO {
     void createGame(Integer gameID, String gameName) throws DataAccessException;
     GameData getGame(int gameID) throws DataAccessException;
-    public HashSet<GameData> listGames(String username) throws DataAccessException;
+    Collection<GameData> listGames() throws DataAccessException;
     void clear() throws DataAccessException;
     void updateGame(GameData g) throws DataAccessException;
 }
