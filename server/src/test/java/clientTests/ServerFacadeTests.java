5476ea59df0bc2254e4217d63bd82e46b079184b
Isaac <it238@byu.edu>
2024-03-16 22:13:27 -0600

    create ServerFacadeTests

--- server/src/test/java/clientTests/ServerFacadeTests.java
+++ server/src/test/java/clientTests/ServerFacadeTests.java
@@ -1,5 +1,7 @@
 package clientTests;
 
+import exception.ResponseException;
+import model.GameData;
 import org.junit.jupiter.api.*;
 import server.Server;
 import server.ServerFacade;
@@ -15,7 +17,7 @@ public class ServerFacadeTests {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
-        facade = new ServerFacade(port);
+        facade = new ServerFacade("http://localhost:" + port);
     }
 
     @AfterAll
@@ -25,8 +27,62 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void positiveRegisterTest() throws ResponseException {
+        facade.register("test", "test", "test");
+    }
+
+    @Test
+    public void negativeRegisterTest() throws ResponseException {
+        facade.register("test", "test", "test");
+    }
+
+    @Test
+    public void positiveLoginTest() throws ResponseException {
+        facade.login("test", "test");
+    }
+
+    @Test
+    public void negativeLoginTest() throws ResponseException {
+        facade.login("test", "test");
+    }
+
+    @Test
+    public void positiveCreateGameTest() throws ResponseException {
+        GameData game = new GameData(0, null, null, "test", null);
+        facade.createGame(game);
+    }
+
+    @Test
+    public void negativeCreateGameTest() throws ResponseException {
+        GameData game = new GameData(0, null, null, "test", null);
+        facade.createGame(game);
+    }
+
+    @Test
+    public void positiveJoinGameTest() throws ResponseException {
+        GameData game = new GameData(0, null, null, "test", null);
+        facade.joinGame(game);
+    }
+
+    @Test
+    public void negativeJoinGameTest() throws ResponseException {
+        GameData game = new GameData(0, null, null, "test", null);
+        facade.joinGame(game);
+    }
+
+    @Test
+    public void clearTest() throws ResponseException {
+        facade.clear();
+    }
+
+    @Test
+    public void positiveListGamesTest() throws ResponseException {
+        facade.listGames();
+    }
+
+    @Test
+    public void negativeListGamesTest() throws ResponseException {
+        facade.listGames();
     }
 
 }
