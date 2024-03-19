8c5395e80bbbe419e7fd45014b668189d8f25863
Isaac <it238@byu.edu>
2024-03-19 09:06:40 -0600

    increase text visibility

--- client/src/main/java/ClientMain.java
+++ client/src/main/java/ClientMain.java
@@ -1,6 +1,9 @@
 import chess.*;
 import client.Repl;
 
+import static client.EscapeSequences.RESET_BG_COLOR;
+import static client.EscapeSequences.SET_TEXT_COLOR_WHITE;
+
 public class ClientMain {
     public static void main(String[] args) {
         var serverUrl = "http://localhost:8080";
@@ -9,6 +12,7 @@ public class ClientMain {
         }
 
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        System.out.print(RESET_BG_COLOR + SET_TEXT_COLOR_WHITE);
         System.out.println("♕ 240 Chess Client: " + piece);
 
         new Repl(serverUrl).run();
