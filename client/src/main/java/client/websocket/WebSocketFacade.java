134ff92996608da145891648b5cb65c1874bb2db
Isaac <it238@byu.edu>
2024-04-10 13:04:03 -0600

    begin WebSocketFacade

---
+++ client/src/main/java/client/websocket/WebSocketFacade.java
@@ -0,0 +1,89 @@
+package client.websocket;
+
+import com.google.gson.Gson;
+import exception.ResponseException;
+import webSocketMessages.userCommands.UserGameCommand;
+import webSocketMessages.serverMessages.ServerMessage;
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+//need to extend Endpoint for websocket to work properly
+public class WebSocketFacade extends Endpoint {
+
+    Session session;
+    NotificationHandler notificationHandler;
+
+
+    public WebSocketFacade(String url, NotificationHandler notificationHandler) throws ResponseException {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/connect");
+            this.notificationHandler = notificationHandler;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            //set message handler
+            this.session.addMessageHandler((MessageHandler.Whole<String>) message -> {
+                ServerMessage notification = new Gson().fromJson(message, ServerMessage.class);
+                notificationHandler.notify(notification);
+            });
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    //Endpoint requires this method, but you don't have to do anything
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
+
+    public void joinPlayer(String authToken) throws ResponseException {
+        try {
+            var action = new UserGameCommand(authToken);
+            this.session.getBasicRemote().sendText(new Gson().toJson(action));
+        } catch (IOException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    public void joinObserver(String authToken) throws ResponseException {
+        try {
+            var action = new UserGameCommand(authToken);
+            this.session.getBasicRemote().sendText(new Gson().toJson(action));
+        } catch (IOException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    public void makeMove(String move) throws ResponseException {
+        try {
+            var action = new UserGameCommand(move);
+            this.session.getBasicRemote().sendText(new Gson().toJson(action));
+        } catch (IOException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    public void leave(String authToken) throws ResponseException {
+        try {
+            var action = new UserGameCommand(authToken);
+            this.session.getBasicRemote().sendText(new Gson().toJson(action));
+            this.session.close();
+        } catch (IOException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    public void resign(String authToken) throws ResponseException {
+        try {
+            var action = new UserGameCommand(authToken);
+            this.session.getBasicRemote().sendText(new Gson().toJson(action));
+        } catch (IOException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+}
