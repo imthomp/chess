1c0a16ed9c4d42fc07fa773addd34852d7dc7091
Isaac <it238@byu.edu>
2024-04-09 22:29:58 -0600

    rename ServerMain to Main

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -2,7 +2,7 @@ import chess.*;
 
 import server.Server;
 
-public class ServerMain {
+public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         Server server = new Server();
