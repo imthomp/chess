04df0bc1e0d0f193cd3288cd3480cb8e960c0ac7
Isaac <it238@byu.edu>
2024-04-12 07:57:29 -0600

    make constructor take in a board, begin to implement highlightLegalMoves

--- client/src/main/java/client/ChessArtist.java
+++ client/src/main/java/client/ChessArtist.java
@@ -1,12 +1,10 @@
 package client;
 
-import chess.ChessBoard;
-import chess.ChessGame;
-import chess.ChessPiece;
-import chess.ChessPosition;
+import chess.*;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Collection;
 
 import static client.EscapeSequences.*;
 
@@ -14,24 +12,73 @@ public class ChessArtist {
 
     //private static final int SQUARE_SIZE_IN_CHARS = 3;
     public static final String EMPTY_SQUARE = "   ";
-    static ChessBoard board = new ChessBoard();
+    public ChessBoard board;
 
-    public ChessArtist() {
-        board.resetBoard();
+    public ChessArtist(ChessBoard board) {
+        this.board = board;
     }
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
         drawBoardPerspectives(out);
     }
 
-    public static void drawBoardPerspectives(PrintStream out) {
-        board.resetBoard();
-        drawChessBoard(out, board, ChessGame.TeamColor.WHITE);
-        drawChessBoard(out, board, ChessGame.TeamColor.BLACK);
+    public void drawBoardPerspectives(PrintStream out) {
+        drawChessBoard(out, ChessGame.TeamColor.WHITE);
+        drawChessBoard(out, ChessGame.TeamColor.BLACK);
     }
 
-    private static void drawChessBoard(PrintStream out, ChessBoard board, ChessGame.TeamColor perspective) {
+    public void highlightLegalMoves(PrintStream out, ChessGame.TeamColor perspective, ChessPosition position) {
+        out.println(SET_TEXT_BOLD);
+        drawRowHeader(out, perspective);
+        if (perspective == ChessGame.TeamColor.WHITE) {
+            for (int row = ChessBoard.BOARD_SIZE; row >= 1; row--) {
+                drawHighlightRow(out, board, perspective, row, position);
+            }
+        } else {
+            for (int row = 1; row <= ChessBoard.BOARD_SIZE; row++) {
+                drawHighlightRow(out, board, perspective, row, position);
+            }
+        }
+        drawRowHeader(out, perspective);
+        out.print(RESET_TEXT_BOLD_FAINT);
+        out.print(SET_TEXT_COLOR_WHITE);
+        out.print(RESET_BG_COLOR);
+    }
+
+    private void drawHighlightRow(PrintStream out, ChessBoard board, ChessGame.TeamColor perspective, int row, ChessPosition position) {
+        drawColHeader(out, row);
+//        var validMoves = board.getPiece(position).validMoves(board, position);
+        if (perspective == ChessGame.TeamColor.WHITE) {
+            for (int col = 1; col <= ChessBoard.BOARD_SIZE; col++) {
+
+                drawHighlightSquare(out, board, row, col, position);
+            }
+        } else {
+            for (int col = ChessBoard.BOARD_SIZE; col >= 1; col--) {
+                drawHighlightSquare(out, board, row, col, position);
+            }
+        }
+        drawColHeader(out, row);
+        out.println(RESET_BG_COLOR);
+    }
+
+    private void drawHighlightSquare(PrintStream out, ChessBoard board, int row, int col, ChessPosition position) {
+        boolean isBlack = (row + col) % 2 == 0;
+        out.print(isBlack ? SET_BG_COLOR_DARK_GREEN : SET_BG_COLOR_GREEN);
+        if (position.getRow() == row && position.getColumn() == col) {
+            out.print(SET_BG_COLOR_YELLOW);
+        }
+        out.print(SET_TEXT_COLOR_RED);
+        ChessPiece piece = board.getPiece(new ChessPosition(row, col));
+        if (piece != null) {
+            out.print(piece.getTeamColor() == ChessGame.TeamColor.WHITE ? SET_TEXT_COLOR_RED : SET_TEXT_COLOR_BLUE);
+        }
+        out.print(piece == null ? EMPTY_SQUARE : " " + piece + " ");
+        out.print(RESET_BG_COLOR);
+    }
+
+    public void drawChessBoard(PrintStream out, ChessGame.TeamColor perspective) {
         out.println(SET_TEXT_BOLD);
         drawRowHeader(out, perspective);
         if (perspective == ChessGame.TeamColor.WHITE) {
@@ -49,12 +96,12 @@ public class ChessArtist {
         out.print(RESET_BG_COLOR);
     }
 
-    private static void drawColHeader(PrintStream out, int row) {
+    private void drawColHeader(PrintStream out, int row) {
         setHeader(out);
         out.print(" " + (row) + " ");
     }
 
-    private static void drawRowHeader(PrintStream out, ChessGame.TeamColor perspective) {
+    private void drawRowHeader(PrintStream out, ChessGame.TeamColor perspective) {
         setHeader(out);
         out.print(EMPTY_SQUARE);
         if (perspective == ChessGame.TeamColor.WHITE) {
@@ -70,7 +117,7 @@ public class ChessArtist {
         out.println(RESET_BG_COLOR);
     }
 
-    private static void drawRow(PrintStream out, ChessBoard board, ChessGame.TeamColor perspective, int row) {
+    private void drawRow(PrintStream out, ChessBoard board, ChessGame.TeamColor perspective, int row) {
         drawColHeader(out, row);
         if (perspective == ChessGame.TeamColor.WHITE) {
             for (int col = 1; col <= ChessBoard.BOARD_SIZE; col++) {
@@ -85,7 +132,7 @@ public class ChessArtist {
         out.println(RESET_BG_COLOR);
     }
 
-    private static void drawSquare(PrintStream out, ChessBoard board, int row, int col) {
+    private void drawSquare(PrintStream out, ChessBoard board, int row, int col) {
         boolean isBlack = (row + col) % 2 == 0;
         out.print(isBlack ? SET_BG_COLOR_BLACK : SET_BG_COLOR_WHITE);
         out.print(SET_TEXT_COLOR_RED);
@@ -97,7 +144,7 @@ public class ChessArtist {
         out.print(RESET_BG_COLOR);
     }
 
-    private static void setHeader(PrintStream out) {
+    private void setHeader(PrintStream out) {
         out.print(SET_BG_COLOR_LIGHT_GREY);
         out.print(SET_TEXT_COLOR_BLACK);
     }
