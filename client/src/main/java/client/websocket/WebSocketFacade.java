1ba88df5475780d489fdc60bb4b47fe5a302ba62
Isaac <it238@byu.edu>
2024-04-12 07:59:28 -0600

    add new server messages, begin to add joinPlayer, joinObserver, makeMove, leave, resign

--- client/src/main/java/client/websocket/WebSocketFacade.java
+++ client/src/main/java/client/websocket/WebSocketFacade.java
@@ -1,9 +1,12 @@
 package client.websocket;
 
+import chess.ChessGame;
+import chess.ChessMove;
 import com.google.gson.Gson;
 import exception.ResponseException;
-import webSocketMessages.userCommands.UserGameCommand;
-import webSocketMessages.serverMessages.ServerMessage;
+import webSocketMessages.userCommands.*;
+import webSocketMessages.serverMessages.*;
+import webSocketMessages.serverMessages.Error;
 
 import javax.websocket.*;
 import java.io.IOException;
@@ -28,8 +31,24 @@ public class WebSocketFacade extends Endpoint {
 
             //set message handler
             this.session.addMessageHandler((MessageHandler.Whole<String>) message -> {
-                ServerMessage notification = new Gson().fromJson(message, ServerMessage.class);
-                notificationHandler.notify(notification);
+                ServerMessage serverMessage = new Gson().fromJson(message, ServerMessage.class);
+                switch (serverMessage.getServerMessageType()) {
+                    case ERROR -> {
+                        ServerMessage error = new Gson().fromJson(message, Error.class);
+                        notificationHandler.notify(error);
+                    }
+                    case NOTIFICATION -> {
+                        ServerMessage notification = new Gson().fromJson(message, Notification.class);
+                        notificationHandler.notify(notification);
+                    }
+                    case LOAD_GAME -> {
+                        ServerMessage loadGame = new Gson().fromJson(message, LoadGame.class);
+                        notificationHandler.notify(loadGame);
+
+                    }
+                }
+
+                //notificationHandler.notify(serverMessage);
             });
         } catch (DeploymentException | IOException | URISyntaxException ex) {
             throw new ResponseException(500, ex.getMessage());
@@ -41,23 +60,48 @@ public class WebSocketFacade extends Endpoint {
     public void onOpen(Session session, EndpointConfig endpointConfig) {
     }
 
-    public void joinPlayer(String authToken) throws ResponseException {
-
+    public void joinPlayer(String authToken, int gameID, ChessGame.TeamColor playerColor) throws ResponseException {
+        JoinPlayer joinPlayer = new JoinPlayer(authToken, gameID, playerColor);
+        try {
+            this.session.getBasicRemote().sendText(new Gson().toJson(joinPlayer));
+        } catch (IOException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
     }
 
-    public void joinObserver(String authToken) throws ResponseException {
-
+    public void joinObserver(String authToken, int gameID) throws ResponseException {
+        JoinObserver joinObserver = new JoinObserver(authToken, gameID);
+        try {
+            this.session.getBasicRemote().sendText(new Gson().toJson(joinObserver));
+        } catch (IOException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
     }
 
-    public void makeMove(String move) throws ResponseException {
-
+    public void makeMove(String authToken, int gameID, ChessMove move) throws ResponseException {
+        MakeMove makeMove = new MakeMove(authToken, gameID, move);
+        try {
+            this.session.getBasicRemote().sendText(new Gson().toJson(makeMove));
+        } catch (IOException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
     }
 
-    public void leave(String authToken) throws ResponseException {
-
+    public void leave(String authToken, int gameID) throws ResponseException {
+        Leave leave = new Leave(authToken, gameID);
+        try {
+            this.session.getBasicRemote().sendText(new Gson().toJson(leave));
+        } catch (IOException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
     }
 
-    public void resign(String authToken) throws ResponseException {
-
+    public void resign(String authToken, int gameID) throws ResponseException {
+        Resign resign = new Resign(authToken, gameID);
+        try {
+            this.session.getBasicRemote().sendText(new Gson().toJson(resign));
+        } catch (IOException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
     }
 }
